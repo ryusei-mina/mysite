0d39bc72238651af615e28f09d2693c32a3a4b77
Ryusei-mina <ryusei.tp105126@icloud.com>
2024-05-16 13:49:03 +0900

    ini

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class PDF(models.Model):
+    file = models.FileField(upload_to='pdfs/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView,PDFDetailView,PDFUploadView
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
+    path('upload/', PDFUploadView.as_view(), name='pdf-upload'),
+    path('<int:pk>/', PDFDetailView.as_view(), name='pdf-detail'),
 ]
